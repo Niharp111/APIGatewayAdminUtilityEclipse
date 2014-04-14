a3fe0b572f6cc05f9cf18e20dcd9836cfcc15278
nileshkatore <nilesh.katore@cognizant.com>
2014-04-14 16:24:52 +0530

    Comma seperated values for providers and IP ranges

--- WebContent/scripts/controllers/client/GetClientInfoController.js
+++ WebContent/scripts/controllers/client/GetClientInfoController.js
@@ -10,11 +10,53 @@ AdminUtilityApp.controller('GetClientInfoController',
                       })
                .success(function(data) {
                               console.log(JSON.stringify(data));
+                              var updatedList = new Array();
+                              var len = data.provisions.length;
+                              console.log(len);
                               $scope.ClientInfo =data;
+                              for (var itr=0; itr < len; itr++) {
+                            	  var objProvision = data.provisions[itr];
+                            	  var strKey = objProvision.provision_key.provider_id;
+                            	  var toSetData = new Object();
+                            	  var blnFound = false;
+                            	  for (var i=0;i < updatedList.length; i++) {
+                            		  var tempData = updatedList[i];
+                            		  if(tempData["key"] == strKey) {
+                            			  blnFound = true;
+                            			  toSetData = tempData;
+                            			  break;
+                            		  }
+                            	  }
+                            	  
+                            	  if(blnFound) {
+                            		  toSetData["key"] = objProvision.provision_key.provider_id;
+                            		  toSetData["value"] = (toSetData["value"] + ", " + objProvision.provision_key.capability_id);
+                            	  } else {
+                            		  toSetData["key"] = objProvision.provision_key.provider_id;
+                            		  toSetData["value"] = objProvision.provision_key.capability_id;
+                            		  updatedList.push(toSetData);
+                            	  }
+                              }
+                              $scope.provisions =updatedList;
                 })
                .error(function(data){
                 console.log('error');
             });
+             
+       
+             
+             $http({
+                 method  : 'GET',
+                 url     : 'admin/client/'+clientId+'/iprange',
+             })
+      .success(function(data) {
+    	  console.log(JSON.stringify(data));
+    	  $scope.ipRange = data;
+      }).error(function(data){
+    	  console.log('error');
+      });
+             
+             
 });
 
 
